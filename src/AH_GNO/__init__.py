fc99cf975925aa2cebd843ef129255e54f072900
Ying Cao <yingcao41@gmail.com>
2026-09-21 15:31:25 +0800

    Add paper-consistent AH-GNO Python package

---
+++ src/AH_GNO/__init__.py
@@ -0,0 +1,16 @@
+"""Public Python interface for AH-GNO."""
+
+from .model import LearnedHorizonGNO, build_model
+from .preprocessing import AreaWeightedNormalizer, compute_node_areas, normalize_coordinates
+from .losses import AreaWeightedHuberLoss, compute_h_star, denormalize_dz
+
+__all__ = [
+    "LearnedHorizonGNO",
+    "build_model",
+    "AreaWeightedNormalizer",
+    "compute_node_areas",
+    "normalize_coordinates",
+    "AreaWeightedHuberLoss",
+    "compute_h_star",
+    "denormalize_dz",
+]
